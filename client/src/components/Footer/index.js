34a4143d4ea7ab5d03c3cfff85df9b253aaba286
davezer <davezer27@gmail.com>
2022-02-16 11:29:16 -0500

    added auth, react app, apollo/client, single command line

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2020 by Dave Oliverio</div>
+    </footer>
+  );
+};
+
+export default Footer;
